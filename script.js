6906c9ff93f6a340e3b870e7eecac27fd8991d98
sambeck87 <sandro_mipb@hotmail.com>
2022-10-24 17:15:48 -0500

    testing the input

---
+++ script.js
@@ -0,0 +1,11 @@
+let click = document.getElementById('button');
+click.addEventListener("click",() => {
+    let input=document.getElementById("value").value;
+
+    console.log(input);
+
+    for(let i=0; i<input;i++){
+        let container=document.createElement('div')
+        for(let j=0; j<input; j++){}
+    }
+})
